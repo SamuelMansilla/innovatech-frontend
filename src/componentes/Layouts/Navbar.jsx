7a10c90c3cf1a5d7c335832c1fbe41b98c19eb03
SAMU <sam.mansilla@duocuc.cl>
2026-06-03 22:36:19 -0400

    test: cambiar color del navbar para pruebas visuales

--- src/componentes/Layouts/Navbar.jsx
+++ src/componentes/Layouts/Navbar.jsx
@@ -1,6 +1,7 @@
 function Navbar() {
   return (
-    <nav className="rounded-xl w-[250px] min-h-[880px] bg-teal-600 text-white sticky top-0 p-4 m-4">
+    // 🌟 Cambiamos bg-teal-600 por bg-indigo-600
+    <nav className="rounded-xl w-[250px] min-h-[880px] bg-indigo-600 text-white sticky top-0 p-4 m-4">
       {/* Logo o título */}
       <h2 className="text-xl font-bold mb-8">Despacho Dashboard</h2>
 
@@ -9,7 +10,8 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            // 🌟 Cambiamos hover:bg-teal-700 por hover:bg-indigo-700
+            className="block font-bold py-2 px-3 hover:bg-indigo-700 rounded"
           >
             Usuarios
           </a>
@@ -17,7 +19,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-indigo-700 rounded"
           >
             Productos
           </a>
@@ -25,7 +27,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-indigo-700 rounded"
           >
             Configuración
           </a>
